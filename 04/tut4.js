bc75c8f9acb6b8f4d94b3b12fdf9b0be220f9629
Madhumitha <madhumithavenu03@gmail.com>
2023-08-07 14:46:43 +0530

    Datatypes in JS

---
+++ 04/tut4.js
@@ -0,0 +1,42 @@
+//primitive data-types
+
+//String
+let name = "Harry";
+console.log("My String is " + name);
+console.log("Data type is " + (typeof name));
+
+//Numbers
+let marks = 34.4;
+console.log('Data type is ' + (typeof marks));
+
+//Boolean
+let isDriver = true;
+console.log('Data type is ' + (typeof isDriver));
+
+//Null
+let nullVar = null;
+console.log('Data type is ' + (typeof nullVar));
+
+//Undefined
+let undef = undefined;
+console.log('Data type is ' + (typeof undef));
+
+//Reference data-Types
+//Arrays
+let myarr = [1,2,3,4,5, false, 'string'];
+console.log('Data type is ' + (typeof myarr));
+
+//Object Literals
+let stMarks = {
+    harry: 89,
+    Shubam: 36,
+    'Rohan Das': 34
+}
+console.log(typeof stMarks);
+function findName() {
+    
+}
+console.log(typeof findName);
+
+let date = new Date();
+console.log(typeof date);
