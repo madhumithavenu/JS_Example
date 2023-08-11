43608adfce1bfa0f5098ec5cad015fb50943ec02
Madhumitha <madhumithavenu03@gmail.com>
2023-08-11 13:03:09 +0530

    Understanding DOM & Creating a Website Layout

---
+++ 12/tut12.js
@@ -0,0 +1,13 @@
+let a = document;
+a = document.all;
+// a = document.body;
+// a = document.forms[0];
+
+// Array.from(a).forEach(function (element){
+//     console.log(element);
+// });
+
+a = document.links[0].href;
+a = document.links[0];
+
+console.log(a);
