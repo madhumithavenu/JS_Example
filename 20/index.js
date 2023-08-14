b962dfeab16d12c6fcde357fe4f7cb773d07406a
Madhumitha <madhumithavenu03@gmail.com>
2023-08-14 12:17:16 +0530

    Local and Session storage in JS

--- 20/index.js
+++ 20/index.js
@@ -1,1 +1,20 @@
-localStorage.setItem('Name','Harry');
+let array = ['adrak', 'pyaz', 'bhindi'];
+
+//Add a key value pair inside the local storage.
+// localStorage.setItem('Name', 'Harry');
+// localStorage.setItem('Name2', 'Rohan');
+// localStorage.setItem('Sabzi',JSON.stringify(array) );
+
+// localStorage.clear();
+
+//clear the perticular key-value pair.
+// localStorage.removeItem('Name');
+
+//Retrives the item from the local Storage.
+let name = localStorage.getItem('Name');
+name = JSON.parse(localStorage.getItem('Sabzi'));
+console.log(name); //harry
+
+// sessionStorage.setItem('sessionName', 'sHarry');
+// sessionStorage.setItem('sessionName2', 'sRohan');
+// sessionStorage.setItem('sessionSabzi',JSON.stringify(array) );
