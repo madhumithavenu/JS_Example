396b113562d6124a5590fb4599d40bc20eab6be4
Madhumitha <madhumithavenu03@gmail.com>
2023-08-17 12:55:01 +0530

    Adding and deleting notes in JS

--- 22/js/app.js
+++ 22/js/app.js
@@ -1,4 +1,5 @@
 console.log("Welcome to notes app. This is app.js");
+showNotes();
 
 let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener("click",function(e){
@@ -15,4 +16,32 @@ addBtn.addEventListener("click",function(e){
     localStorage.setItem("notes", JSON.stringify(notesObj));
     addTxt.value = "";
     console.log(notesObj);
+    showNotes();
 })
+
+function showNotes(){
+    let notes = localStorage.getItem("notes");
+    if(notes == null){
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes);
+    }
+    let html = "";
+    notesObj.forEach(function(element, index){
+        html += `<div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Note ${index + 1}</h5>
+            <p class="card-text">${element}</p>
+            <button href="#" class="btn btn-primary">Delete Note</button>
+        </div>
+    </div>`
+    });
+    let notesElm = document.getElementById('notes');
+    if(notesObj.length != 0){
+        notesElm.innerHTML = html;
+    }
+    else{
+        notesElm.innerHTML = `Nothing to show! use "Add a Note" section above to add notes.`;
+    }
+}
