7606ca4538673c4be7e7af21c6f6b198260f73d2
Madhumitha <madhumithavenu03@gmail.com>
2023-08-17 12:41:28 +0530

    Working on local Storage

--- 22/js/app.js
+++ 22/js/app.js
@@ -1,1 +1,18 @@
 console.log("Welcome to notes app. This is app.js");
+
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener("click",function(e){
+
+    let addTxt = document.getElementById("addTxt");
+    let notes = localStorage.getItem("notes");
+    if(notes == null){
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = "";
+    console.log(notesObj);
+})
