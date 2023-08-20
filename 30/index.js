7b18641134495e287d8ae3394d2278d91167cf75
Madhumitha <madhumithavenu03@gmail.com>
2023-08-20 19:24:57 +0530

    ES6 Inheritance in JS

--- 30/index.js
+++ 30/index.js
@@ -15,6 +15,30 @@ class Employee{
     }
 }
 
+class Programmer extends Employee{
+    constructor(givenName, givenExperience, givenDivision,language, github){
+        super(givenName, givenExperience, givenDivision);
+        this.language = language;
+        this.github = github;
+    }
+    favoriteLanguage(){
+        if(this.language == 'python'){
+            return 'Python';
+        }
+        else{
+            return 'JavaScript';
+        }
+    }
+    static multiply(a,b){
+        return a * b;
+    }
+}
+
 // harry = new Employee("harry", 56,"Division");
 // console.log(harry.joiningYear());
-console.log(Employee.add(34,5));
+// console.log(Employee.add(34,5));
+rohan = new Programmer("Rohan",3,"Lays","Go","Rohan420")
+console.log(rohan.favoriteLanguage());
+console.log(Programmer.multiply(5,7));
+
+
