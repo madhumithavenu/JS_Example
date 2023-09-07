262d6049e63b6acd793ce95f16f0938416dcbca3
Madhumitha <madhumithavenu03@gmail.com>
2023-09-07 15:13:42 +0530

    Creating constructor

--- 33-p/index.js
+++ 33-p/index.js
@@ -1,1 +1,24 @@
 console.log("Hello");
+
+//Constructor
+function Book(name,author,type){
+    this.name = name;
+    this.author = author;
+    this.type = type;
+}
+
+//Display constructor
+function Display(){
+
+}
+
+//Add methods to display prototypes
+
+//Add submit event listener to library form
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit',libraryFormSubmit);
+
+function libraryFormSubmit(e){
+    e.preventDefault();
+    console.log('You have submitted library form');
+}
