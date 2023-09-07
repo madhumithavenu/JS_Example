86be09b81cd12e041ae9508ee3a84c41dfb377d0
Madhumitha <madhumithavenu03@gmail.com>
2023-09-07 15:58:10 +0530

    Storing all data to data storage

--- 33-p/index.js
+++ 33-p/index.js
@@ -1,18 +1,22 @@
-console.log("Hello");
+console.log("This is index.js");
+// Todos"
+// 1. Store all the data to the localStorage
+// 2. Give another column as an option to delete the book
+// 3. Add a scroll bar to the view
 
-//Constructor
+// Constructor
 function Book(name, author, type) {
     this.name = name;
     this.author = author;
     this.type = type;
 }
 
-//Display constructor
+// Display Constructor
 function Display() {
 
 }
 
-//Add methods to display prototypes
+// Add methods to display prototype
 Display.prototype.add = function (book) {
     console.log("Adding to UI");
     tableBody = document.getElementById('tableBody');
@@ -23,21 +27,23 @@ Display.prototype.add = function (book) {
                     </tr>`;
     tableBody.innerHTML += uiString;
 }
-//Implement the clear function
+
+// Implement the clear function
 Display.prototype.clear = function () {
     let libraryForm = document.getElementById('libraryForm');
     libraryForm.reset();
 }
-//Implement the validate function
+
+// Implement the validate function
 Display.prototype.validate = function (book) {
-    if (book.name.length < 2 || book.author.length < 2){
-        return false;
+    if (book.name.length < 2 || book.author.length < 2) {
+        return false
     }
-    else{
+    else {
         return true;
     }
 }
-Display.prototype.show = function (type) {
+Display.prototype.show = function (type, displayMessage) {
     let message = document.getElementById('message');
     message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
                             <strong>Messge:</strong> ${displayMessage}
@@ -48,19 +54,19 @@ Display.prototype.show = function (type) {
     setTimeout(function () {
         message.innerHTML = ''
     }, 2000);
+
 }
 
-//Add submit event listener to library form
+
+// Add submit event listener to libraryForm
 let libraryForm = document.getElementById('libraryForm');
 libraryForm.addEventListener('submit', libraryFormSubmit);
 
 function libraryFormSubmit(e) {
-    console.log('You have submitted library form');
+    console.log('YOu have submitted library form');
     let name = document.getElementById('bookName').value;
     let author = document.getElementById('author').value;
     let type;
-    // fiction programming, cooking
-
     let fiction = document.getElementById('fiction');
     let programming = document.getElementById('programming');
     let cooking = document.getElementById('cooking');
@@ -79,6 +85,7 @@ function libraryFormSubmit(e) {
     console.log(book);
 
     let display = new Display();
+
     if (display.validate(book)) {
 
         display.add(book);
@@ -89,5 +96,6 @@ function libraryFormSubmit(e) {
         // Show error to the user
         display.show('danger', 'Sorry you cannot add this book');
     }
+
     e.preventDefault();
 }
