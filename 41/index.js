540159631b9bd1e92bc40e7f978e00e68eb71593
Madhumitha <madhumithavenu03@gmail.com>
2023-09-13 12:00:09 +0530

    Arrow function in JS

---
+++ 41/index.js
@@ -0,0 +1,9 @@
+// const haary = () => {
+//     console.log('This is the best person');
+// }
+// harry();
+
+// const greet = () => "Good Morning";
+// const greet = () => ({name: "harry"});
+const greet = (name, ending) => "Good Morning " + name + ending;
+console.log(greet('harry', ' bye'));
