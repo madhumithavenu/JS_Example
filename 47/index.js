90e884a9b6ff2ed8afb9bf755ffb531850b102b6
Madhumitha <madhumithavenu03@gmail.com>
2023-09-22 12:42:11 +0530

    Regular Expressions - Character sets

--- 47/index.js
+++ 47/index.js
@@ -22,3 +22,9 @@ str = "harhar1r4r5r bhai";
 
 let result = regex.exec(str);
 console.log("The result from exec is ", result);
+
+if (regex.test(str)) {
+    console.log(`The string ${str} matches the expression ${regex.source}`);
+} else {
+    console.log(`The string ${str} does not matches the expression ${regex.source}`);
+}
