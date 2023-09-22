eb8192f7a8298696bd3c24a1320c927128764a77
Madhumitha <madhumithavenu03@gmail.com>
2023-09-22 10:24:31 +0530

    Regular expression and related function

--- 45/index.js
+++ 45/index.js
@@ -13,16 +13,16 @@ let result = reg.exec(s);
 // result = reg.exec(s);
 // console.log(result);//['harry', index: 39, input: 'This is great code with harry and also harry bhai', groups: undefined]
 
-/we can use multiple exec with global flag
-// if(result){
+// we can use multiple exec with global flag
+if(result){
 console.log(result);
 console.log(result.input);
 console.log(result.index);
-// }
+}
 
 //2.test()- returns true or false
 let result2 = reg.test(s);
-// console.log(result2); --> This will only print true if the "reg" is there in a string "a"
+// console.log(result2); --> This will only print true if the "reg" is there in a string "s"
 
 // 3. match() - It will return an array of results or null.
 let result3 = s.match(reg) //--> This is right
