eecdfbbbb2233d82da3363f8f585d435513966b0
Madhumitha <madhumithavenu03@gmail.com>
2023-09-22 11:48:59 +0530

    Regular Expressions - Character sets

--- 46/index.js
+++ 46/index.js
@@ -2,6 +2,10 @@ let regex = /harrygfkh/;
 // Metacharacter symbols
 regex = /^harrdc/; //^ means expression will match if string starts with
 regex = /harry$/; //$ means expression will match if string ends with
+regex = /h./; //. matches any one character
+regex = /h*rry/; //* matches any 0 or more characters
+regex = /ha?rryi?/; //? after character means that character is optional
+regex = /h\*rry/;
 
 // let str = "harry is a good boy and harry is code with harry";
 // str = "hsdbdkvubdkuvb is hadrry"
