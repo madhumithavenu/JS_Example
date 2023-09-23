26bdbe3f4b95edadc234cd74c8e45f34b312ec1e
Madhumitha <madhumithavenu03@gmail.com>
2023-09-23 13:13:54 +0530

    Working on submit button

--- 49-p/index.js
+++ 49-p/index.js
@@ -63,3 +63,27 @@ phone.addEventListener('blur', () => {
     }
 })
 
+let submit = document.getElementById('submit');
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    console.log('You clicked on submit');
+    console.log(validEmail, validUser, validPhone);
+
+    // Submit your form here
+    if (validEmail && validUser && validPhone) {
+        let failure = document.getElementById('failure');
+
+        console.log('Phone, email and user are valid. Submitting the form');
+        let success = document.getElementById('success');
+        success.classList.add('show');
+        
+
+    }
+    else {
+        console.log('One of Phone, email or user are not valid. Hence not submitting the form. Please correct the errors and try again');
+        let failure = document.getElementById('failure');
+        failure.classList.add('show');
+        
+    }
+})
