df2b63ecef059fadd94d94629b277932ced87b63
Madhumitha <madhumithavenu03@gmail.com>
2023-09-23 14:41:39 +0530

    Working on iterators

---
+++ 50/index.js
@@ -0,0 +1,24 @@
+function fruitsIterator(values) {
+    let nextIndex = 0;
+    //we will return an object
+    return {
+        next: function () {
+            if (nextIndex < values.length) {
+                //we will return this object
+                return {
+                    value: values[nextIndex++],
+                    done: false
+                }
+            } else {
+                //We will return below object with only done
+                return {
+                    done: true
+                }
+            }
+        }
+    }
+}
+
+
+const myArray = ['Apples', 'Grapes', 'Oranges', 'Bhindi'];
+console.log("My array is ", myArray);
