5da32bf8fcd979567d9c410b4e296236f841f765
Madhumitha <madhumithavenu03@gmail.com>
2023-09-23 12:33:54 +0530

    Checking validation

--- 49-p/index.js
+++ 49-p/index.js
@@ -3,24 +3,27 @@ const email = document.getElementById('email');
 const phone = document.getElementById('phone');
 
 // console.log(name,email,phone);
-name.addEventListener('blur', () => {
+name.addEventListener('blur',()=>{
     console.log("name is blurred");
     //Validate name here
-    let regex = /^[a-zA-Z]([0-9a-zA-z]){0,10}/;
+    let regex = /^[a-zA-Z]([0-9a-zA-z]){2,10}$/;
     let str = name.value;
-    console.log(regex, str);
-    if (regex.test(str)) {
-        console.log('it matched');
+    console.log(regex,str);
+    if(regex.test(str)){
+        console.log('name is valid');
+        name.classList.remove('is-invalid');
     }
-    else {
+    else{
         console.log('no match');
+        name.classList.add('is-invalid');
     }
 })
-email.addEventListener('blur', () => {
+email.addEventListener('blur',()=>{
     console.log("email is blurred");
     //Validate email here
 })
-phone.addEventListener('blur', () => {
+
+phone.addEventListener('blur',()=>{
     console.log("phone is blurred");
     //Validate phone here
 })
