2be361d859fb7cc36f399d5d33b3beb2e46f780f
Madhumitha <madhumithavenu03@gmail.com>
2023-09-23 11:03:34 +0530

    Regular Expressions

--- 46/index.js
+++ 46/index.js
@@ -3,12 +3,12 @@ let regex = /harrygfkh/;
 regex = /^harrdc/; //^ means expression will match if string starts with
 regex = /harry$/; //$ means expression will match if string ends with
 regex = /h./; //. matches any one character
-regex = /h*rry/; //* matches any 0 or more characters
+regex = /har*y/; //* matches any 0 or more characters
 regex = /ha?rryi?/; //? after character means that character is optional
 regex = /h\*rry/;
 
 // let str = "harry is a good boy and harry is code with harry";
-
+0
 // str = "hsdbdkvubdkuvb is hadrry"
 // str = "harryi"
 let str = "harry means codewith";
