c154ed60a88dffcf47d3bea2bcdad0d614210242
Madhumitha <madhumithavenu03@gmail.com>
2023-09-25 12:10:38 +0530

    Adding profiles to the page

--- 53-p/index.js
+++ 53-p/index.js
@@ -71,5 +71,19 @@ function nextCV(){
     const currentCandidate = candidates.next().value;
     let image = document.getElementById('image');
     let profile = document.getElementById('profile');
+    if(currentCandidate != undefined){
+    image.innerHTML = `<img src='${currentCandidate.image}'>`;
+    profile.innerHTML = `<ul class="list-group">
+    <li class="list-group-item">Name: ${currentCandidate.name}</li>
+    <li class="list-group-item">${currentCandidate.age} years old</li>
+    <li class="list-group-item">Lives in ${currentCandidate.city}</li>
+    <li class="list-group-item">Primarily works on ${currentCandidate.language}</li>
+    <li class="list-group-item">Uses ${currentCandidate.framework} framework</li>
+  </ul>`;
+    }
+    else{
+        alert('End of candidate applications');
+        window.location.reload();
+    }
 
 }
