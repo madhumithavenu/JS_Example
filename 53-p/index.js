944dd7f91f45999b6c181e8e891e633e8c9f8bab
Madhumitha <madhumithavenu03@gmail.com>
2023-09-25 12:13:02 +0530

    Adding content to page

--- 53-p/index.js
+++ 53-p/index.js
@@ -1,4 +1,4 @@
-console.log("This is my index.js");
+
 // Data is an array of objects which contains information about the candidates
 const data = [
     {
@@ -49,31 +49,31 @@ const data = [
 
 
 // CV Iterator
-function cvIterator(profiles){
-    let nextIndex=0;
+function cvIterator(profiles) {
+    let nextIndex = 0;
     return {
-        next: function(){
-            return nextIndex<profiles.length ?
-            {value: profiles[nextIndex++], done: false} :
-            {done: true}
+        next: function () {
+            return nextIndex < profiles.length ?
+                { value: profiles[nextIndex++], done: false } :
+                { done: true }
         }
     };
 }
-
 const candidates = cvIterator(data);
 
+nextCV();
 // Button listener for next button
 const next = document.getElementById('next');
 next.addEventListener('click', nextCV);
 
 
-function nextCV(){
+function nextCV() {
     const currentCandidate = candidates.next().value;
     let image = document.getElementById('image');
     let profile = document.getElementById('profile');
-    if(currentCandidate != undefined){
-    image.innerHTML = `<img src='${currentCandidate.image}'>`;
-    profile.innerHTML = `<ul class="list-group">
+    if (currentCandidate != undefined) {
+        image.innerHTML = `<img src='${currentCandidate.image}'>`;
+        profile.innerHTML = `<ul class="list-group">
     <li class="list-group-item">Name: ${currentCandidate.name}</li>
     <li class="list-group-item">${currentCandidate.age} years old</li>
     <li class="list-group-item">Lives in ${currentCandidate.city}</li>
@@ -81,9 +81,8 @@ function nextCV(){
     <li class="list-group-item">Uses ${currentCandidate.framework} framework</li>
   </ul>`;
     }
-    else{
+    else {
         alert('End of candidate applications');
         window.location.reload();
     }
-
 }
