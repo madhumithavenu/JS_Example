02f4a97c676067a559144800c368e7be6742c627
Madhumitha <madhumithavenu03@gmail.com>
2023-09-25 12:06:25 +0530

    Adding profiles

---
+++ 53-p/index.js
@@ -0,0 +1,32 @@
+console.log("This is my index.js");
+// Data is an array of objects which contains information about the candidates
+const data = [
+    {
+        name: 'Rohan Das',
+        age: 18,
+        city: 'Kolkata',
+        language: 'Python',
+        framework: 'Django',
+        image: 'https://randomuser.me/api/portraits/men/51.jpg'
+    },
+
+    {
+        name: 'Shubham Sharma',
+        age: 28,
+        city: 'Bangalore',
+        language: 'JavaScript',
+        framework: 'Angular',
+        image: 'https://randomuser.me/api/portraits/men/54.jpg'
+    },
+
+    {
+        name: 'Camella Cabello',
+        age: 18,
+        city: 'Kolkata',
+        language: 'Python',
+        framework: 'Django',
+        image: 'https://randomuser.me/api/portraits/women/55.jpg'
+    },
+
+    
+]
