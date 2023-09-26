ee3d2703553adb2126cfea2f19485d641af4a3c3
Madhumitha <madhumithavenu03@gmail.com>
2023-09-26 11:51:13 +0530

    Js symbols

---
+++ 56/index.js
@@ -0,0 +1,6 @@
+// Symbols
+const sym1 = Symbol('My identifier');
+const sym2 = Symbol('My identifier');
+// console.log('Symbol is ', sym1);
+// console.log('Type of symbol is ', typeof sym1);
+console.log(sym1 === sym2);
