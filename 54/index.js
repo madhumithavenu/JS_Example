096f907bb2692ed899f6ff29f785d16a854f666f
Madhumitha <madhumithavenu03@gmail.com>
2023-09-26 11:00:40 +0530

    Get the size of the map

--- 54/index.js
+++ 54/index.js
@@ -11,3 +11,15 @@ myMap.set(key2, 'This is a blank obj')
 myMap.set(key3, 'This is an empty function')
 
 console.log(myMap);
+
+// Getting a value from a map
+let value1 = myMap.get(key3);
+console.log(value1);
+
+// Get the size of the map
+console.log(myMap.size);
+
+// We can loop using for..of to get key and values
+for (let [key, value] of myMap) {
+    console.log(key, value);
+}
