4b4bb372fc294fa255fe9d1832fadf0bbee6eb53
Madhumitha <madhumithavenu03@gmail.com>
2023-09-26 11:33:53 +0530

    Adding values to the set

---
+++ 55/index.js
@@ -0,0 +1,14 @@
+//Set stores unique values
+const mySet = new Set(); //Initialize an empty set
+console.log('The set looks like :', mySet);
+
+//Adding values to the set
+mySet.add('this');
+mySet.add('My name');
+mySet.add('this');
+mySet.add('that');
+mySet.add(345);
+mySet.add(true);
+mySet.add(false);
+mySet.add('that2');
+console.log('The set looks like this now:', mySet);
