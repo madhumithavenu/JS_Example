87b4a1aa6d6eba16f29085692fa732cc8e82ef65
Madhumitha <madhumithavenu03@gmail.com>
2023-09-26 11:34:33 +0530

    Using a constructor

--- 55/index.js
+++ 55/index.js
@@ -12,3 +12,17 @@ mySet.add(true);
 mySet.add(false);
 mySet.add('that2');
 console.log('The set looks like this now:', mySet);
+
+//Use a constructor to iniatize the set
+let mySet2 = new Set([1, 45, 'this', false, { a: 4, b: 8 }, 'this']);
+console.log('New Set :', mySet2);
+
+console.log(mySet.size); //get the size of the set
+
+console.log(mySet.has(345)); //check whether set has 345 or not
+
+console.log('before removal', mySet.has('that2'));
+
+mySet.delete('that2'); //Remove an element from the set
+
+console.log('after removal', mySet.has('that2'));
