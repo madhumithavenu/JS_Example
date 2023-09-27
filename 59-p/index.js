afebfa83252f66653fed080d36e369a4f5d440da
Madhumitha <madhumithavenu03@gmail.com>
2023-09-27 16:16:56 +0530

    If the user clicks on + button, add more parameters

--- 59-p/index.js
+++ 59-p/index.js
@@ -28,3 +28,33 @@ jsonRadio.addEventListener('click', () => {
     document.getElementById('requestJsonBox').style.display = 'block';
     document.getElementById('parametersBox').style.display = 'none';
 })
+
+// If the user clicks on + button, add more parameters
+let addParam = document.getElementById('addParam');
+addParam.addEventListener('click', () => {
+    let params = document.getElementById('params');
+    let string = `<div class="form-row my-2">
+                    <label for="url" class="col-sm-2 col-form-label">Parameter ${addedParamCount + 2}</label>
+                    <div class="col-md-4">
+                        <input type="text" class="form-control" id="parameterKey${addedParamCount + 2}" placeholder="Enter Parameter ${addedParamCount + 2} Key">
+                    </div>
+                    <div class="col-md-4">
+                        <input type="text" class="form-control" id="parameterValue${addedParamCount + 2}" placeholder="Enter Parameter ${addedParamCount + 2} Value">
+                    </div>
+                    <button class="btn btn-primary deleteParam"> - </button>
+                    </div>`;
+    // Convert the element string to DOM node
+    let paramElement = getElementFromString(string);
+    params.appendChild(paramElement);
+    // Add an event listener to remove the parameter on clicking - button
+    let deleteParam = document.getElementsByClassName('deleteParam');
+    for (item of deleteParam) {
+        item.addEventListener('click', (e) => {
+            // TODO: add a confirmation box to confirm parameter deletion
+            e.target.parentElement.remove();
+        })
+    }
+    addedParamCount++;
+
+});
+
