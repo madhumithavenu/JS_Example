4734269740ceb99361d549137a40cbb4c41ff215
Madhumitha <madhumithavenu03@gmail.com>
2023-09-27 16:15:59 +0530

    Hide the parameters box initially

---
+++ 59-p/index.js
@@ -0,0 +1,25 @@
+console.log('This is my project 6 from JavaScript course');
+
+// Utility functions:
+// 1. Utility function to get DOM element from string
+function getElementFromString(string) {
+    let div = document.createElement('div');
+    div.innerHTML = string;
+    return div.firstElementChild;
+}
+
+// Initialize no of parameters
+let addedParamCount = 0;
+
+// Hide the parameters box initially
+let parametersBox = document.getElementById('parametersBox');
+parametersBox.style.display = 'none';
+
+// If the user clicks on params box, hide the json box
+let paramsRadio = document.getElementById('paramsRadio');
+paramsRadio.addEventListener('click', () => {
+    document.getElementById('requestJsonBox').style.display = 'none';
+    document.getElementById('parametersBox').style.display = 'block';
+})
+
+
