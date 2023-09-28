d2ae3a37c34b057f69a619723e1b0d79a8acfe7e
Madhumitha <madhumithavenu03@gmail.com>
2023-09-28 13:26:02 +0530

    Functions to calculate

--- 60/index.js
+++ 60/index.js
@@ -1,6 +1,7 @@
 let screen = document.getElementById('screen');
 buttons = document.querySelectorAll('button');
 let screenValue = '';
+
 for (item of buttons) {
     item.addEventListener('click', (e) => {
         buttonText = e.target.innerText;
